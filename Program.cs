fbdd2d20e4b27c2dcf9242bec9e69931d58f6fe0
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-09-14 12:03:17 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SayHi();
+            int num = Add(8, 7);
+            Console.WriteLine(num);
+        
+        }
+
+        
+        static void SayHi()
+        {
+            Console.WriteLine("Hi!");
+            
+        }
+        
+
+        static int Add(int x, int y)
+        {
+            int z = x + y;
+            return z;
+           
+
+        }
+
+    }
+}
